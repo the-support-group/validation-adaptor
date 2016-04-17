6c552edf65e105ed98c77092e6e7f973da70aca4
abdul <abdul@easyfundraising.org.uk>
2016-04-17 17:53:29 +0100

    Validation adaptor

--- src/ValidationAdaptor.php
+++ src/ValidationAdaptor.php
@@ -6,11 +6,18 @@ use TheSupportGroup\Common\ValidationInterop\ValidationProviderInterface;
 
 class ValidationAdaptor implements validationProviderInterface
 {
+    /**
+     * Set the validator object.
+     */
     public function __construct($validator)
     {
         $this->validator = $validator;
     }
 
+    /**
+     * @param string $ruleName The rule name.
+     * @param array $arguments The arguments for the rule.
+     */
     public function rule($ruleName, array $arguments = [])
     {
         $this->validator->rule($ruleName, $arguments);
